77d237cffd2224ed1b98cc2321a1c3855b75586e
Emil Nielsen <emilqz@gmail.com>
2020-04-24 15:56:16 +0200

    Statistics of literals and variables in trees

--- expressionStatistics.ml
+++ expressionStatistics.ml
@@ -9,7 +9,25 @@ let nodes = function
         | OperatorApplication (_, _) -> 2
         | ConditionalApplication (_, _, _) -> 3
 
-let full_gen = make (expression_gen Integer global_scope 6)
+let rec size_literal_nodes = function 
+    | None -> 0
+    | Some node -> match node with 
+      | Literal l -> 1
+      | Variable (_, _) -> 0
+      | OperatorApplication (n1, n2) -> size_literal_nodes (Some (n1)) + size_literal_nodes (Some(n2))
+      | ConditionalApplication (n1, n2, n3) -> size_literal_nodes (Some(n1)) + size_literal_nodes (Some(n2)) + size_literal_nodes (Some(n3))
+
+let rec size_variable_nodes = function 
+      | None -> 0
+      | Some node -> match node with 
+        | Literal l -> 0
+        | Variable (_, _) -> 1
+        | OperatorApplication (n1, n2) -> size_variable_nodes (Some (n1)) + size_variable_nodes (Some(n2))
+        | ConditionalApplication (n1, n2, n3) -> size_variable_nodes (Some(n1)) + size_variable_nodes (Some(n2)) + size_variable_nodes (Some(n3))
+       
+let fuel = 6     
+
+let full_gen = make (expression_gen Integer global_scope fuel)
 
 (* let expression_gen_statistics = make ~stats:[("expressions", nodes)] (expression_gen Integer global_scope 2) *)
 let integer_test =     
@@ -22,12 +40,29 @@ let integer_test =
   let integer_gen = set_collect
     (fun n -> node (nodes n))
     (full_gen) in
-  Test.make ~count:10000 ~name:"integer tree_nodes"
+  Test.make ~count:10000 ~name:"integer root"
      integer_gen
      (fun _ -> true)
 
+let exp_gen = expression_gen Integer global_scope fuel
+
+let expression_gen_literal_statistics = 
+   make ~stats:[("literal with fuel " ^ string_of_int fuel, size_literal_nodes)]
+   (exp_gen)
+
+let expression_gen_variable_statistics = 
+   make ~stats:[("variable with fuel " ^ string_of_int fuel, size_variable_nodes)]
+   (exp_gen)   
+
+let literal_test = Test.make ~count:10000 ~name:"literal statistics three"
+   expression_gen_literal_statistics (fun _ -> true)
+
+let variable_test = Test.make ~count:10000 ~name:"variable statistics three"
+   expression_gen_variable_statistics (fun _ -> true)
 
 let _ = QCheck_runner.run_tests ~verbose:true
   [
     integer_test;
+    literal_test;
+    variable_test;
   ]
